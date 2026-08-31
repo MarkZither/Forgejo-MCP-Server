205652871651b6501ffdd8883e46394f51912fee
MarkZither <mark.burton@zither-it.co.uk>
2026-08-31 21:48:05 +0200

    Test

--- .husky/pre-commit.ps1
+++ .husky/pre-commit.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
-Write-Host ">>> Husky pre-commit.ps1 is running" -ForegroundColor Cyan
+Write-Host ">>> Husky pre-commit.ps1 is running." -ForegroundColor Cyan
 dotnet husky run --group pre-commit
